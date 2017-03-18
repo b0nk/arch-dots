a558c54c8f484418b351784f06632ffb071df419
b0nk <fsantos.pt@hotmail.com>
2017-03-18 16:59:00 +0000

    edited both scripts and some dots

---
+++ backup.sh
@@ -0,0 +1,20 @@
+# backup some shi- stuff
+
+# conky
+cp -f -u ~/.config/conky/conky.conf  ./
+
+# zsh
+cp -f -u ~/.zshrc ./
+cp -f -u ~/.zprofile ./
+
+# x
+cp -f -u ~/.xinitrc ./
+
+# tint2
+cp -f -u ~/.config/tint2/tint2rc ./
+
+# openbox
+cp -f -u ~/.config/openbox/autostart ./
+
+# wallpaper
+cp -f -u ~/JYV.png ./
---
+++ restore.sh
@@ -0,0 +1,20 @@
+# backup some shi- stuff
+
+# conky
+cp -f -u ./conky.conf ~/.config/conky/conky.conf
+
+# zsh
+cp -f -u ./.zshrc ~/.zshrc
+cp -f -u ./.zprofile ~/.zprofile
+
+# x
+cp -f -u ./.xinitrc ~/.xinitrc 
+
+# tint2
+cp -f -u ./tint2rc ~/.config/tint2/tint2rc
+
+# openbox
+cp -f -u ./autostart ~/.config/openbox/autostart
+
+# wallpaper
+cp -f -u ./JYV.png ~/JYV.png
